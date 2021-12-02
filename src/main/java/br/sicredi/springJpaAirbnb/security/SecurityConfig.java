5796c4a82b17cc12cde5f379f3f7e423afa56205
Maria Manoela <manoelamvc@hotmail.com>
2021-12-02 10:24:10 -0300

    Finishing application login

--- src/main/java/br/sicredi/springJpaAirbnb/security/SecurityConfig.java
+++ src/main/java/br/sicredi/springJpaAirbnb/security/SecurityConfig.java
@@ -1,5 +1,22 @@
 package br.sicredi.springJpaAirbnb.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    Authentication
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("Manoela").password("{noop}123")
+                .roles("LOCATARIO").and().withUser("Manu").password("{noop}123")
+                .roles("LOCADOR", "LOCATARIO");
+    }
+//    Authorization
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests().antMatchers("/usuarios/**", "/imoveis/tipo/**")
+                .hasRole("LOCATARIO").antMatchers("/**").hasRole("LOCADOR").and()
+                .csrf().disable().headers().frameOptions().disable();
+    }
 }
